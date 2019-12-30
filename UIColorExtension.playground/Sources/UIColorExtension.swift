f8d0c1cbd45422c516da708f01146eee614b652d
Satoru-PriChan <other_usage@icloud.com>
2019-12-30 14:09:04 +0900

    Implemented Decorator for UILabel.

---
+++ UIColorExtension.playground/Sources/UIColorExtension.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+public extension UIColor {
+    //To initialize UIColor from HTML color code.
+    convenience init(hex: UInt32) {
+        let divisor = CGFloat(255)
+        let red = CGFloat((hex & 0xFF0000) >> 16) / divisor
+        let green = CGFloat((hex & 0x00FF00) >> 8) / divisor
+        let blue = CGFloat(hex & 0x0000FF) / divisor
+        
+        self.init(red: red, green: green, blue: blue, alpha: 1)
+    }
+}
